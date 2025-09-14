eb5f73aa1d774a4984db46509be71a1113c9ef2b
Activity_Lifecycle <natan.aw95@gmail.com>
2025-09-14 17:56:05 +0700

    Commit No. 3 - use dynamic fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
